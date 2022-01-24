05bd58125220ce91307a6f4e959eae742abc4aac
m.a.dudka <madudka.dev@gmail.com>
2022-01-24 22:27:28 +0300

    Add main window

---
+++ app/src/main/java/com/madudka/popquiz/MainActivity.java
@@ -0,0 +1,20 @@
+package com.madudka.popquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Убираем статусную строку
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
